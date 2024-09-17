70b8afd965d545dd1a89d307610e85b50cb10aa5
YamazakiYusuke <yusuke38098@gmail.com>
2024-09-18 08:18:25 +0900

    ssg

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+import ResponseModel from "@/models/ResponseModel";
 import localFont from "next/font/local";
-import ResponseModel from "@/pages/ResponseModel";
+import Image from "next/image";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,7 +20,7 @@ export async function getServerSideProps() {
   return { props: { data } }
 }
 
-export default function Home({ data } : {data: ResponseModel}) {
+export default function Home({ data }: { data: ResponseModel }) {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
